f3241473d06ff899121460ae3c2802231fc60e93
Fabulousxu <1292248830@qq.com>
2024-06-22 17:41:29 +0800

    Initial commit

---
+++ server/src/test/kotlin/com/example/renyibang/RenYiBangApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.renyibang
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RenYiBangApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
