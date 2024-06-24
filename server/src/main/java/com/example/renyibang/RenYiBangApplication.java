edbfae62fc8b957c963c503435b1eaa576bfa749
Fabulousxu <1292248830@qq.com>
2024-06-24 10:56:40 +0800

    重构了整个初始项目
    1. 采用react来构建前端
    2. 采用java-spring-maven来构建服务器

---
+++ server/src/main/java/com/example/renyibang/RenYiBangApplication.java
@@ -0,0 +1,13 @@
+package com.example.renyibang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenYiBangApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RenYiBangApplication.class, args);
+  }
+
+}
