cd853ab7c70ebf9697903a5976cf4f11ebed27d1
GiggleWang <2386181363@qq.com>
2024-06-26 11:18:11 +0800

    将order中的图片设置为跑马灯

--- app/src/page/order_page.jsx
+++ app/src/page/order_page.jsx
@@ -5,39 +5,39 @@ import {Link} from 'react-router-dom';
 import data from '../util/order_data'
 
 export default function OrderPage() {
-  const columns = [{
-    title: '任务标题',
-    dataIndex: 'title',
-    key: 'title',
-    render: (text, record) => <Link to={`/order/${record.id}`}>{text}</Link>,
-  }, {
-    title: '发起人', dataIndex: 'initiator', key: 'initiator',
-  }, {
-    title: '接收人', dataIndex: 'receiver', key: 'receiver',
-  }, {
-    title: '发起时间', dataIndex: 'startTime', key: 'startTime',
-  }, {
-    title: '当前状态', dataIndex: 'status', key: 'status', render: (status) => {
-      let color = '';
-      switch (status) {
-        case '进行中':
-          color = 'blue';
-          break;
-        case '已完成':
-          color = 'green';
-          break;
-        case '已取消':
-          color = 'red';
-          break;
-        default:
-          color = 'gray';
-      }
-      return <Tag color={color}>{status}</Tag>;
-    },
-  },];
+    const columns = [{
+        title: '任务标题',
+        dataIndex: 'title',
+        key: 'title',
+        render: (text, record) => <Link to={`/order/${record.id}`}>{text}</Link>,
+    }, {
+        title: '发起人', dataIndex: 'initiator', key: 'initiator',
+    }, {
+        title: '接收人', dataIndex: 'receiver', key: 'receiver',
+    }, {
+        title: '发起时间', dataIndex: 'startTime', key: 'startTime',
+    }, {
+        title: '当前状态', dataIndex: 'status', key: 'status', render: (status) => {
+            let color = '';
+            switch (status) {
+                case '进行中':
+                    color = 'blue';
+                    break;
+                case '已完成':
+                    color = 'green';
+                    break;
+                case '已取消':
+                    color = 'red';
+                    break;
+                default:
+                    color = 'gray';
+            }
+            return <Tag color={color}>{status}</Tag>;
+        },
+    },];
 
 
-  return (<BasicLayout page='order'>
-    <Table columns={columns} dataSource={data}/>
-  </BasicLayout>);
+    return (<BasicLayout page='order'>
+        <Table columns={columns} dataSource={data}/>
+    </BasicLayout>);
 }
