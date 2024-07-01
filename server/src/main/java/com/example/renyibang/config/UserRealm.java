916325e00e6d6af6ebab5bdf82dae7156d8abe98
GiggleWang <2386181363@qq.com>
2024-07-01 14:18:20 +0800

    完成了shiro的整体安全框架搭建，并且对/user/login有了整体的框架

---
+++ server/src/main/java/com/example/renyibang/config/UserRealm.java
@@ -0,0 +1,26 @@
+package com.example.renyibang.config;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+
+public class UserRealm extends AuthorizingRealm {
+    //授权
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        System.out.println("执行了=>授权doGetAuthorizationInfo");
+        return null;
+    }
+
+    //认证
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        System.out.println("执行了=>认证doGetAuthenticationInfo");
+        return null;
+    }
+}
