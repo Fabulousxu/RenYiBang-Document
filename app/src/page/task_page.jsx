0790fb13787a8b9c183f03cbf08dc0de34a55801
Fabulousxu <1292248830@qq.com>
2024-07-01 15:22:30 +0800

    前端任务列表和服务列表接入后端

--- app/src/page/task_page.jsx
+++ app/src/page/task_page.jsx
@@ -3,43 +3,52 @@ import BasicLayout from "../component/basic_layout";
 import {searchTask} from "../service/task";
 import {useEffect, useState} from "react";
 import {totalEntry} from "../component/item_list";
+import {message} from "antd";
 
 export default function TaskPage() {
   const [total, setTotal] = useState(0)
   const [taskList, setTaskList] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
   const [keyword, setKeyword] = useState('')
+  const [priceRange, setPriceRange] = useState([0, -1])
+  const [timeRange, setTimeRange] = useState(['', ''])
+  const [orderValue, setOrderValue] = useState('time')
+  const [messageApi, contextHolder] = message.useMessage()
+
   useEffect(() => {
-    searchTask('', totalEntry, 0).then(res => {
+    searchTask(keyword, totalEntry, 0, 'time', ['', ''], [0, -1]).then(res => {
       setTotal(res.total)
       setTaskList(res.items)
-    }).catch(err => {
-    })
+    }).catch(err => messageApi.open({type: 'error', content: err}))
   }, [])
   return (<BasicLayout page='task'>
     <ItemList
       title={`任务 ${total}条`}
+      ratingTitle='任务评分: '
       placeholder='请输入任务关键词或用户关键词来搜索相关任务'
       value={keyword}
       onSearch={value => {
         setKeyword(value)
-        searchTask(value, totalEntry, 0).then(res => {
+        searchTask(value, totalEntry, 0, orderValue, timeRange, priceRange).then(res => {
           setTotal(res.total)
           setTaskList(res.items)
           setCurrentPage(1)
-        }).catch(err => {
-        })
+        }).catch(err => messageApi.open({type: 'error', content: err}))
       }}
+      onChangePriceRange={value => setPriceRange(value)}
+      onChangeTimeRange={value => setTimeRange(value)}
+      onChangeOrder={value => setOrderValue(value)}
       list={taskList}
       total={total}
       currentPage={currentPage}
       onChange={(page, pageSize) => {
-        searchTask('', pageSize, page - 1).then(res => {
+        searchTask(keyword, totalEntry, page - 1, orderValue, timeRange, priceRange).then(res => {
           setTotal(res.total)
           setTaskList(res.items)
           setCurrentPage(page)
-        }).catch(err => {
-        })
+        }).catch(err => messageApi.open({type: 'error', content: err}))
+      }}
+      onCollect={index => {
       }}
     />
   </BasicLayout>)
--- app/src/service/task.js
+++ app/src/service/task.js
@@ -1,25 +1,9 @@
-export async function searchTask(keyword, pageSize, pageIndex) {
-  // 模拟得到的信息
-  let item = {
-    taskId: 1,
-    title: '任务',
-    cover: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-    description: '任务内容',
-    price: 2000,
-    user: {
-      userId: 1,
-      avatar: 'https://api.dicebear.com/7.x/miniavs/svg?seed=8',
-      username: '用户',
-      rating: 98
-    }
-  }, items = [], number = 0
-  if (pageIndex === 0 || pageIndex === 1) number = 24
-  else if (pageIndex === 2) number = 2
-  for (let i = 0; i < number; ++i) items.push(item)
+import {apiURL, get} from "./util";
 
-  let getResponse = {total: 50, items}
-  for (let item of getResponse.items) item.url = `/task/${item.taskId}`
-  return getResponse
+export async function searchTask(keyword, pageSize, pageIndex, order, timeRange, priceRange) {
+  let res = await get(`${apiURL}/task/search?keyword=${keyword}&pageSize=${pageSize}&pageIndex=${pageIndex}&order=${order}&timeBegin=${timeRange[0]}&timeEnd=${timeRange[1]}&priceLow=${priceRange[0]}&priceHigh=${priceRange[1]}`)
+  for (let task of res.items) task.url = `/task/${task.taskId}`
+  return res
 }
 
 export async function getTask(taskId) {
@@ -28,13 +12,7 @@ export async function getTask(taskId) {
     taskId: 1,
     title: '任务',
     description: '任务内容',
-    images: [
-      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
-    ],
+    images: ['https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png', 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png', 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png', 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png', 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'],
     price: 2000,
     user: {
       userId: 1,
@@ -51,17 +29,14 @@ export async function getTask(taskId) {
 export async function getTaskComment(taskId, pageSize, pageIndex, order) {
   // 模拟得到的信息
   let item = {
-    content: '评论内容',
-    rating: 70,
-    user: {
+    content: '评论内容', rating: 70, user: {
       userId: 1,
       avatar: 'https://api.dicebear.com/7.x/miniavs/svg?seed=8',
       username: '用户',
       rating: 98
     }
   }, items = [], number = 0
-  if (pageIndex === 0 || pageIndex === 1) number = 10
-  else if (pageIndex === 2) number = 2
+  if (pageIndex === 0 || pageIndex === 1) number = 10; else if (pageIndex === 2) number = 2
   for (let i = 0; i < number; ++i) items.push(item)
 
   let getResponse = {total: 22, items}
@@ -71,17 +46,14 @@ export async function getTaskComment(taskId, pageSize, pageIndex, order) {
 export async function getTaskMessage(taskId, pageSize, pageIndex, order) {
   // 模拟得到的信息
   let item = {
-    content: '留言内容',
-    rating: 70,
-    user: {
+    content: '留言内容', rating: 70, user: {
       userId: 1,
       avatar: 'https://api.dicebear.com/7.x/miniavs/svg?seed=8',
       username: '用户',
       rating: 98
     }
   }, items = [], number = 0
-  if (pageIndex === 0 || pageIndex === 1 || pageIndex === 2) number = 10
-  else if (pageIndex === 3) number = 6
+  if (pageIndex === 0 || pageIndex === 1 || pageIndex === 2) number = 10; else if (pageIndex === 3) number = 6
   for (let i = 0; i < number; ++i) items.push(item)
 
   let getResponse = {total: 36, items}
