2f34d3fa443a45d53d562e7bb4bbe0630ec2dfeb
Fabulousxu <1292248830@qq.com>
2024-07-16 09:09:35 +0800

    更新了第二次迭代报告

--- api/ChatApi/src/main/java/com/renyibang/chatapi/dao/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.renyibang.chatapi.dao;
-
-import com.renyibang.chatapi.entity.Message;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends MongoRepository<Message, String> {}
